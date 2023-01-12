cf1225d7aae11e155fa7643962b3b48d333d5e37
HongTr <truongminhhong0306@gmail.com>
2023-01-12 08:09:35 +0700

    Add name age to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'users/show'
   get 'static_pages/home'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_scope :user do 
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
